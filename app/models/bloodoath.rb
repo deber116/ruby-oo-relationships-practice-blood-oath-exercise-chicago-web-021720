d81763f2182c28e63f0c90ce36cd7a71b78e620b
deber116 <deber116@gmail.com>
2020-02-25 13:51:27 -0600

    Done.

--- app/models/bloodoath.rb
+++ app/models/bloodoath.rb
@@ -18,6 +18,9 @@ class BloodOath
         @initiation_date.strftime("%F")
     end
 
+    #initiation date is saved as a TIme class. This is based on an integer of seconds
+    #since 01/01/1970, so using min_by will give you the instance associated with
+    #the lowest integer (earliest initiation date/time)
     def self.first_oath
         result = self.all.min_by {|bo| bo.initiation_date}
         result.follower
